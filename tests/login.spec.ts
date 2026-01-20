b20a9752adbc70b75ab12692c7831c7be9700c4d
Rahima Irfan <rahimairfan.official@gmail.com>
2026-01-20 11:51:07 +0500

    add new testcase

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,17 @@
 import { test, expect } from '@playwright/test';
-test('Login page should load', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/login');
-  await expect(page.locator('h2')).toHaveText('Login Page');
+test('Login with valid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+  await expect(page.locator('.title')).toHaveText('Products');
 });
+
+test('Login with invalid credentials', async ({page}) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.fill('#user-name','wrong_user');
+    await page.fill('#password', "wrong_password");
+    await page.click("#login-button");
+    await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Username and password do not match any user in this service');
+});
+
